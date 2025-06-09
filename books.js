5c35f296813cc46c81b2486b2516cc861aa09fdb
Jordan <jordan.atk00@gmail.com>
2025-06-09 16:06:28 -0400

    extra html issue not solved

--- books.js
+++ books.js
@@ -6,17 +6,17 @@ const books = getBooks();
 console.log(filter)
 
 if (filter === 'LOW_TO_HIGH') {
-    books.sort((a,b) => a.originalPrice - b.originalPrice);
+    books.sort ((a,b) => (a.salePrice || a.originalPrice) - (b.salePrice || b.originalPrice));
 }
 else if (filter === 'HIGH_TO_LOW') {
-    books.sort((a,b) => b.originalPrice - a.originalPrice )
+    books.sort((a,b) => (b.salePrice || b.originalPrice) - (a.salePrice|| a.originalPrice))
 }
 else if (filter === 'RATING') {
     books.sort((a,b) => b.rating - a.rating);
 }
 
-
-const booksHtml = books.map(book => {
+const booksHtml = books
+.map(book => {
  return `<div class="book">
 <figure class="book__img--wrapper">
     <img class="book__img" src="${book.url}" alt="">
@@ -28,15 +28,24 @@ const booksHtml = books.map(book => {
    ${ratingsHTML(book.rating)}
 </div>
 <div class="book__price">
+  ${priceHTML(book.originalPrice, book.salePrice)}
+  $59.95
+</div>
+</div>;
     <span>$${book.originalPrice.toFixed(2)}</span> 
 </div>
 </div>`;
 })
 .join("");
 
-console.log(booksHtml);
-
 booksWrapper.innerHTML = booksHtml;
+}
+
+function priceHTML(originalPrice, salePrice) {
+  if (!salePrice) {
+    return `$${originalPrice.toFixed(2)}`
+  }
+   return   `<span class="book__price--normal">$${originalPrice.toFixed(2)}</span> $${salePrice.toFixed(2)}`
 }
 
 function ratingsHTML(rating) {
